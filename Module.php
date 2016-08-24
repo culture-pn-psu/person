b03c5c23b3e748d6ec2536097b1e0015b426cace
Madone <ahamad.jedu@gmail.com>
2016-08-24 16:19:08 +0700

    Add file & change nanespace

---
+++ Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace culturePnPsu\person;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'culturePnPsu\person\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
